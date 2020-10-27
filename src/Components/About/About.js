443f9b5e6f6d1d610a75ce3a11613c4355d0564b
lizbarnett0 <lizcroper@gmail.com>
2020-10-27 18:52:57 -0500

    Added testimonial carousel and about text

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -2,11 +2,20 @@ import React from 'react';
 
 const About = () => {
     return (
-        <div>
-            <h1>About</h1>
-            <p></p>
-        </div>
-    );
+			<div>
+				<h1>About</h1>
+				<p>
+					Have you ever had to host a fancy dinner party and had no idea where
+					to start? Need help finding recipes and and cocktails that all your
+					guests will enjoy? Worried that the dinner conversation will take a
+					turn towards taboo subjects like politics and religion? Look no
+					further than P3 - PartyPlannerPRO. P3 is an easy to navigate web
+					application that will provide you with a cocktail recipe, a dinner
+					recipe, and a list of conversation topics to make sure your party is a
+					night to remember!{' '}
+				</p>
+			</div>
+		);
 };
 
 export default About;
--- src/Components/Testimonials/Testimonials.js
+++ src/Components/Testimonials/Testimonials.js
@@ -1,11 +1,44 @@
 import React from 'react';
+import Carousel from 'react-bootstrap/Carousel';
 
 const Testimonials = () => {
-    return (
-        <div>
-           <h1>Testimonials</h1> 
-        </div>
-    );
+	return (
+		<div>
+			<h1>Testimonials</h1>
+			<Carousel>
+				<Carousel.Item interval={5000}>
+					<h1>THE MOST AMAZING APP EVER!</h1>
+					<Carousel.Caption>
+						<p>
+							Nulla vitae elit libero, a pharetra augue mollis interdum.Nulla
+							vitae elit libero, a pharetra augue mollis interdum.
+						</p>
+						<h3>Andrea G.</h3>
+					</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item interval={5000}>
+					<h1>Make us some more apps please</h1>
+					<Carousel.Caption>
+						<p>
+							Nulla vitae elit libero, a pharetra augue mollis interdum.Nulla
+							vitae elit libero, a pharetra augue mollis interdum.
+						</p>
+						<h3>Craig R.</h3>
+					</Carousel.Caption>
+				</Carousel.Item>
+				<Carousel.Item interval={5000}>
+					<h1>I would be lost without PPP</h1>
+					<Carousel.Caption>
+						<p>
+							Nulla vitae elit libero, a pharetra augue mollis interdum.Nulla
+							vitae elit libero, a pharetra augue mollis interdum.
+						</p>
+						<h3>Margot M.</h3>
+					</Carousel.Caption>
+				</Carousel.Item>
+			</Carousel>
+		</div>
+	);
 };
 
 export default Testimonials;
