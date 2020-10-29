364423ee6b31e2d24f799e2d1d371b9639364af1
lizbarnett0 <lizcroper@gmail.com>
2020-10-29 15:25:39 -0500

    updated about pages and testimonials

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -4,28 +4,29 @@ import './about.scss'
 const About = () => {
     return (
 			<div className='about-container'>
-				<h1>About</h1>
+				<div className='about-text'>
+					<h1>About</h1>
+					<h3>
+						Have you ever had to host a fancy dinner party and had no idea where
+						to start?
+					</h3>
+					<h3>
+						Need help finding recipes and and cocktails that all your guests
+						will enjoy? No matter their dietary restrictions or drinking
+						preferences.
+					</h3>
+					<h3>
+						Worried that the dinner conversation will take a turn towards taboo
+						subjects like politics and religion?
+					</h3>
 
-				<h3>
-					Have you ever had to host a fancy dinner party and had no idea where
-					to start?
-				</h3>
-				<h3>
-					Need help finding recipes and and cocktails that all your guests will
-					enjoy? No matter their dietary restrictions or drinking preferences.
-				</h3>
-				<h3>
-					Worried that the dinner conversation will take a turn towards taboo
-					subjects like politics and religion?
-				</h3>
-
-				<h3>
-					Don't fret! Party Planner Pro has it under control!
-				</h3>
-				<h2>
-					Party Planner pro is an easy to navigate web application that will provide you with a cocktail recipe, a dinner
-					recipe, and a list of conversation topics to make sure your party is anight to remember!
-				</h2>
+					<h3>Don't fret! Party Planner Pro has it under control!</h3>
+					<h3>
+						Party Planner pro is an easy to navigate web application that will
+						provide you with a cocktail recipe, a dinner recipe, and a list of
+						conversation topics to make sure your party is anight to remember!
+					</h3>
+				</div>
 			</div>
 		);
 };
--- src/Components/Testimonials/Testimonials.js
+++ src/Components/Testimonials/Testimonials.js
@@ -4,44 +4,46 @@ import './testimonials.scss';
 const Testimonials = () => {
 	return (
 		<div className='testimonial-container'>
-			<h1>Testimonials</h1>
-			<div>
-				<h3>THE MOST AMAZING APP EVER!</h3>
-				<p>
-					That is saying a lot considering I created Facebook. I love this app,
-					it lets me pretty much outsource the heavy lifting for all the dinner
-					parties I am having. If only this app could tell me how to dress for a
-					dinner party since all I own are hoodies...
-				</p>
-				<h5>-Mark Z.</h5>
-			</div>
+			<div className='testimonial-text'>
+				<h1>Testimonials</h1>
+				<div>
+					<h3>THE MOST AMAZING APP EVER!</h3>
+					<p>
+						That is saying a lot considering I created Facebook. I love this
+						app, it lets me pretty much outsource the heavy lifting for all the
+						dinner parties I am having. If only this app could tell me how to
+						dress for a dinner party since all I own are hoodies...
+					</p>
+					<h4>-Mark Z.</h4>
+				</div>
 
-			<div>
-				<h3>This app is a lifesaver.</h3>
-				<p>
-					I was completely lost planning dinner parties now that I don't have
-					the White House staff taking care of that for me. With PPP, I can
-					entertain important people and send them home impressed and satisfied!
-				</p>
-				<h5>-Barack O.</h5>
-			</div>
-			<div>
-				<h3>Maybe I'm Biased, but ...</h3>
-				<p>
-					My daughter is amazing and so is her app. I don't know what more I
-					need to say.
-				</p>
-				<iframe
-					src='https://giphy.com/embed/8OGSUyxhyBzvwozf3l'
-					width='480'
-					height='270'
-					frameBorder='0'
-					class='giphy-embed'
-					tite='krisjennergif'
-					allowFullScreen></iframe>
+				<div>
+					<h3>This app is a lifesaver.</h3>
+					<p>
+						I was completely lost planning dinner parties now that I don't have
+						the White House staff taking care of that for me. With PPP, I can
+						entertain important people and send them home impressed and
+						satisfied!
+					</p>
+					<h4>-Barack O.</h4>
+				</div>
+				<div>
+					<h3>Maybe I'm Biased, but ...</h3>
+					<p>
+						My daughter is amazing and so is her app. I don't know what more I
+						need to say.
+					</p>
+					<iframe
+						src='https://giphy.com/embed/8OGSUyxhyBzvwozf3l'
+						width='480'
+						height='270'
+						frameBorder='0'
+						class='giphy-embed'
+						tite='krisjennergif'
+						allowFullScreen></iframe>
 
-
-				<h5>-Mom</h5>
+					<h4>-Mom</h4>
+				</div>
 			</div>
 		</div>
 	);
