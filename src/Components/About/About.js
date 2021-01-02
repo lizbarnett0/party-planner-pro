22dcbce1cfff124dafc658159b8c76b4eb3bedf7
lizbarnett0 <lizcroper@gmail.com>
2021-01-01 20:04:49 -0600

    more mobile formatting

--- src/Components/About/About.js
+++ src/Components/About/About.js
@@ -6,25 +6,25 @@ const About = () => {
 			<div className='about-container'>
 				<div className='about-text'>
 					<h1>About</h1>
-					<h3>
+					<p>
 						Have you ever had to host a fancy dinner party and had no idea where
 						to start?
-					</h3>
-					<h3>
+					</p>
+					<p>
 						Need help finding recipes and and cocktails that all your guests
 						will enjoy? 
-					</h3>
-					<h3>
+					</p>
+					<p>
 						Worried that the dinner conversation will take a turn towards taboo
 						subjects like politics and religion?
-					</h3>
+					</p>
 
-					<h3>Don't fret! Party Planner Pro has it under control!</h3>
-					<h3>
-						Party Planner pro is an easy to navigate web application that will
+					<p>Don't fret! Party Planner Pro has it under control!</p>
+					<p>
+						Party Planner Pro is an easy to navigate web application that will
 						provide you with a cocktail recipe, a dinner recipe, and a list of
 						conversation topics to make sure your party is a night to remember!
-					</h3>
+					</p>
 				</div>
 			</div>
 		);
